3b580066fc07f18998c65ae255f4daf1f75b9172
darisasubhash <subbusubbu8193@gmail.com>
2026-02-05 22:21:39 +0530

    [Subhash] : UC-5 Adding multiple users

--- src/main/java/com/addressbook/AddressBook.java
+++ src/main/java/com/addressbook/AddressBook.java
@@ -1,35 +1,40 @@
 package com.addressbook;
 
-public class AddressBook {
+import java.util.ArrayList;
+import java.util.List;
 
-        private Contact contact;
+public class AddressBook {
+        private List<Contact> contactList = new ArrayList<>();
 
         public void addContact(Contact contact) {
-            this.contact = contact;
-            System.out.println("Contact added successfully!");
+            contactList.add(contact);
+            System.out.println("Contact added successfully ");
         }
         public void editContact(String name, String address, String city,
                                 String state, String zip,
                                 String phoneNumber, String email) {
-
-            if (contact != null && contact.getFirstName().equalsIgnoreCase(name)) {
-                contact.setAddress(address);
-                contact.setCity(city);
-                contact.setState(state);
-                contact.setZip(zip);
-                contact.setPhoneNumber(phoneNumber);
-                contact.setEmail(email);
-                System.out.println("Contact updated successfully!");
-            } else {
-                System.out.println("Contact not found!");
+            for (Contact contact : contactList) {
+                if (contact.getFirstName().equalsIgnoreCase(name)) {
+                    contact.setAddress(address);
+                    contact.setCity(city);
+                    contact.setState(state);
+                    contact.setZip(zip);
+                    contact.setPhoneNumber(phoneNumber);
+                    contact.setEmail(email);
+                    System.out.println("Contact updated successfully ");
+                    return;
+                }
             }
+            System.out.println("Contact not found ");
         }
-    public void deleteContact(String name) {
-        if (contact != null && contact.getFirstName().equalsIgnoreCase(name)) {
-            contact = null;
-            System.out.println("Contact deleted successfully!");
-        } else {
-            System.out.println("Contact not found!");
+        public void deleteContact(String name) {
+            for (Contact contact : contactList) {
+                if (contact.getFirstName().equalsIgnoreCase(name)) {
+                    contactList.remove(contact);
+                    System.out.println("Contact deleted successfully ");
+                    return;
+                }
+            }
+            System.out.println("Contact not found ");
         }
-    }
 }
--- src/main/java/com/addressbook/AddressBookMain.java
+++ src/main/java/com/addressbook/AddressBookMain.java
@@ -7,25 +7,29 @@ public class AddressBookMain {
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
             AddressBook addressBook = new AddressBook();
+
+            String firstName, lastName, address, city, state, zip, phoneNumber, email;
+            String choice;
+
             //UC-2 Add contact
+            System.out.println("Add Single Contact ");
             System.out.print("Enter First Name: ");
-            String firstName = scanner.nextLine();
+            firstName = scanner.nextLine();
             System.out.print("Enter Last Name: ");
-            String lastName = scanner.nextLine();
+            lastName = scanner.nextLine();
             System.out.print("Enter Address: ");
-            String address = scanner.nextLine();
+            address = scanner.nextLine();
             System.out.print("Enter City: ");
-            String city = scanner.nextLine();
+            city = scanner.nextLine();
             System.out.print("Enter State: ");
-            String state = scanner.nextLine();
+            state = scanner.nextLine();
             System.out.print("Enter Zip: ");
-            String zip = scanner.nextLine();
+            zip = scanner.nextLine();
             System.out.print("Enter Phone Number: ");
-            String phoneNumber = scanner.nextLine();
+            phoneNumber = scanner.nextLine();
             System.out.print("Enter Email: ");
-            String email = scanner.nextLine();
-            Contact contact = new Contact(firstName,lastName,address,city,state,zip,phoneNumber,email);
-            addressBook.addContact(contact);
+            email = scanner.nextLine();
+            addressBook.addContact(new Contact(firstName,lastName,address,city,state,zip,phoneNumber,email));
 
             //UC-3 edit contact
             System.out.print("\nEnter First Name to edit contact: ");
@@ -48,5 +52,30 @@ public class AddressBookMain {
             System.out.print("\nEnter First Name to delete contact: ");
             String deleteName = scanner.nextLine();
             addressBook.deleteContact(deleteName);
+
+            //UC-5 Adding Multiple users
+            do {
+                System.out.print("\nEnter First Name: ");
+                firstName = scanner.nextLine();
+                System.out.print("Enter Last Name: ");
+                lastName = scanner.nextLine();
+                System.out.print("Enter Address: ");
+                address = scanner.nextLine();
+                System.out.print("Enter City: ");
+                city = scanner.nextLine();
+                System.out.print("Enter State: ");
+                state = scanner.nextLine();
+                System.out.print("Enter Zip: ");
+                zip = scanner.nextLine();
+                System.out.print("Enter Phone Number: ");
+                phoneNumber = scanner.nextLine();
+                System.out.print("Enter Email: ");
+                email = scanner.nextLine();
+                addressBook.addContact(new Contact(firstName,lastName,address,city,state,zip,phoneNumber,email));
+
+                System.out.print("Do you want to add another contact? (yes/no): ");
+                choice = scanner.nextLine();
+            } while (choice.equalsIgnoreCase("yes"));
+
         }
 }
